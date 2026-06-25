4dd9923961099c0fb85b0795f84a9438a84580f1
DT <dydyqja12345@naver.com>
2026-06-25 09:30:05 +0000

    상호작용 UI 블루프린트 연결
    git-svn-id: https://svna.gameinjae.kr/svn/GA7thFinal_LostSignal/5_Project/LostSignal@413 30c2e7c1-fbdc-f34c-824a-6268ce7535d9

--- Source/LostSignal/UI/LSInteractHintWidget.cpp
+++ Source/LostSignal/UI/LSInteractHintWidget.cpp
@@ -3,6 +3,7 @@
 
 void ULSInteractHintWidget::UpdateHintInfo(const FText& ObjectName, const FText& KeyName)
 {
-	if (ObjectNameText) ObjectNameText->SetText(ObjectName);
+	// 오브젝트 이름은 숨기고 입력 키(F)만 표시한다.
+	if (ObjectNameText) ObjectNameText->SetVisibility(ESlateVisibility::Collapsed);
 	if (KeyText) KeyText->SetText(KeyName);
 }
